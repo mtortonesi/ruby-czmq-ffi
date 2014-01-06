c23e76f012c47af7ea963a3bb4ddb05bd31a212e
Mauro Tortonesi <mauro.tortonesi@unife.it>
2014-01-06 15:32:51 +0100

    Added FFI definitions of zctx-, zbeacon-, and zsocket-related functions.

--- lib/ruby-czmq-ffi.rb
+++ lib/ruby-czmq-ffi.rb
@@ -3,4 +3,5 @@ require 'ffi'
 CZMQ_FFI_LIB_PATH = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift(CZMQ_FFI_LIB_PATH) unless $LOAD_PATH.include?(CZMQ_FFI_LIB_PATH)
 
+require 'ruby-czmq-ffi/version'
 require 'ruby-czmq-ffi/libczmq'
--- lib/ruby-czmq-ffi/libczmq.rb
+++ lib/ruby-czmq-ffi/libczmq.rb
@@ -13,4 +13,31 @@ module LibCZMQ
 
   attach_function :zctx_new, [], :pointer
   attach_function :zctx_destroy, [:pointer], :void
+  attach_function :zctx_set_io_threads, [:pointer, :int], :void
+  attach_function :zctx_set_linger, [:pointer, :int], :void
+  attach_function :zctx_set_pipehwm, [:pointer, :int], :void
+  attach_function :zctx_set_sendhwm, [:pointer, :int], :void
+  attach_function :zctx_set_recvhwm, [:pointer, :int], :void
+  attach_function :zctx_underlying, [:pointer], :pointer
+
+  attach_function :zbeacon_new, [:int], :pointer
+  attach_function :zbeacon_destroy, [:pointer], :void
+  attach_function :zbeacon_hostname, [:pointer], :pointer
+  attach_function :zbeacon_set_interval, [:pointer, :int], :void
+  attach_function :zbeacon_noecho, [:pointer], :void
+  attach_function :zbeacon_publish, [:pointer, :pointer, :size_t], :void
+  attach_function :zbeacon_silence, [:pointer], :void
+  attach_function :zbeacon_subscribe, [:pointer, :pointer, :size_t], :void
+  attach_function :zbeacon_unsubscribe, [:pointer], :void
+  attach_function :zbeacon_socket, [:pointer], :pointer
+
+  attach_function :zsocket_new, [:pointer, :int], :pointer
+  attach_function :zsocket_destroy, [:pointer], :void
+  attach_function :zsocket_bind, [:pointer, :string, :varargs], :void
+  attach_function :zsocket_unbind, [:pointer, :string, :varargs], :void
+  attach_function :zsocket_connect, [:pointer, :string, :varargs], :void
+  attach_function :zsocket_disconnect, [:pointer, :string, :varargs], :void
+  attach_function :zsocket_poll, [:pointer, :int], :bool
+  attach_function :zsocket_type_str, [:pointer], :pointer
+  attach_function :zsocket_sendmem, [:pointer, :pointer, :size_t, :int], :int
 end
